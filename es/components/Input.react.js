1761d0ad98704906400c916c2e80fbef70500ff9
dendi <dendi@GridQube-fpamb>
2024-07-19 12:37:46 +1000

    added npm prepare, updated .gitignore file and removed build files from git commit

--- es/components/Input.react.js
+++
@@ -1,8 +0,0 @@
-import _extends from "@babel/runtime/helpers/extends";
-import cx from 'classnames';
-import React from 'react';
-const Input = /*#__PURE__*/React.forwardRef((props, ref) => /*#__PURE__*/React.createElement("input", _extends({}, props, {
-  className: cx('rbt-input-main', props.className),
-  ref: ref
-})));
-export default Input;
